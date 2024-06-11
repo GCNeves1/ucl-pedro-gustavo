56ffd9c44d4724a231be9ad33b575196402fcc2e
pedrohreinholz <pedrohreinholz@gmail.com>
2024-06-11 18:14:31 -0300

    criaçao da classe vendas e correçao do program

--- ProjetoAlgoritmos/Program.cs
+++ ProjetoAlgoritmos/Program.cs
@@ -3,24 +3,27 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 
-internal class Program
+namespace Padaria
 {
-    private static void Main(string[] args)
+
+    public class Program
     {
-        // Listas
-        // Início das listas
-        List<cadastroCliente> listaDeClientes = new List<cadastroCliente>();
-        List<estoque> listaDeProdutos = new List<estoque>();
-
-        // Fim das listas
-
-        // voids
-        //inicio dos voids
-        void menu()
+        public static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Bem vindo:");
-            Console.WriteLine(@" #####      ##     ####       ##     #####     ####      ##
+            // Listas
+            // Início das listas
+            List<cadastroCliente> listaDeClientes = new List<cadastroCliente>();
+            List<estoque> listaDeProdutos = new List<estoque>();
+
+            // Fim das listas
+
+            // voids
+            //inicio dos voids
+            void menu()
+            {
+                Console.Clear();
+                Console.WriteLine("Bem vindo:");
+                Console.WriteLine(@" #####      ##     ####       ##     #####     ####      ##
  ##  ##    ####    ## ##     ####    ##  ##     ##      ####
  ##  ##   ##  ##   ##  ##   ##  ##   ##  ##     ##     ##  ##
  #####    ######   ##  ##   ######   #####      ##     ######
@@ -29,160 +32,178 @@ internal class Program
  ##       ##  ##   ####     ##  ##   ##  ##    ####    ##  ##
 ");
 
-            Console.WriteLine("Digite 1 para registrar uma venda");
-            Console.WriteLine("Digite 2 para cadastrar cliente");
-            Console.WriteLine("Digite 3 para dar entrada no estoque");
-            Console.WriteLine("Digite 4 para cadastrar uma fornecedora");
-            Console.WriteLine("Digite 5 para ver clientes cadastrados");
-            Console.WriteLine("Digite 6 para ver produtos cadastrados");
-            Console.WriteLine("Digite -1 para ver sair do sistema");
+                Console.WriteLine("Digite 1 para registrar uma venda");
+                Console.WriteLine("Digite 2 para cadastrar cliente");
+                Console.WriteLine("Digite 3 para dar entrada no estoque");
+                Console.WriteLine("Digite 4 para cadastrar uma fornecedora");
+                Console.WriteLine("Digite 5 para ver clientes cadastrados");
+                Console.WriteLine("Digite 6 para ver produtos cadastrados");
+                Console.WriteLine("Digite -1 para ver sair do sistema");
 
-        }
-
-        void clientes()
-        {
-            Console.Clear();
-            Console.WriteLine("**************************");
-            Console.WriteLine("LISTA DE CLIENTES ADICIONADOS");
-            Console.WriteLine("**************************");
-            Console.WriteLine();
-
-            for (int i = 0; i < listaDeClientes.Count; i++)
-            {
-                Console.WriteLine($"Cliente {(i + 1)}:");
-                Console.WriteLine($"Nome: {listaDeClientes[i].GetNome()}");
-                Console.WriteLine($"Telefone: {listaDeClientes[i].GetTelefone()}");
-                Console.WriteLine($"Email: {listaDeClientes[i].GetEmail()}");
-                Console.WriteLine();
             }
 
-            Console.WriteLine();
-            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-            Console.ReadKey();
-            Console.Clear();
-        }
-
-        void cadastro()
-        {
-            Console.Clear();
-            // Criação de um novo cliente
-            cadastroCliente novoCliente = new cadastroCliente();
-
-            // Nome
-            Console.WriteLine("Qual cliente você gostaria de registrar ?");
-            novoCliente.SetNome(Console.ReadLine());
-            Console.WriteLine();
-
-            // Telefone
-            Console.WriteLine("Qual o telefone do cliente:");
-            novoCliente.SetTelefone(Console.ReadLine());
-            Console.WriteLine();
-
-            // Email
-            Console.WriteLine("Qual o email do cliente:");
-            novoCliente.SetEmail(Console.ReadLine());
-            Console.WriteLine();
-
-            // Adiciona o novo cliente à lista de clientes
-            listaDeClientes.Add(novoCliente);
-        }
-
-        void adicionarProduto()
-        {
-            Console.Clear();
-
-            estoque novoProduto = new estoque();
-
-            // Nome do produto
-            Console.WriteLine("Qual produto você gostaria de adicionar?");
-            novoProduto.SetProduto(Console.ReadLine());
-            Console.WriteLine();
-
-            // Valor do produto
-            Console.WriteLine("Qual o valor do produto?");
-            novoProduto.SetValor(float.Parse(Console.ReadLine()));
-            Console.WriteLine();
-            Console.Clear();
-            Console.WriteLine($"Você adicionou o produto: {novoProduto.GetProduto()} o valor dele é R${novoProduto.GetValor()}");
-            Thread.Sleep(5000);
-
-
-            listaDeProdutos.Add(novoProduto);
-        }
-
-        void produtos()
-        {
-            Console.Clear();
-            Console.WriteLine("**************************");
-            Console.WriteLine("LISTA DE PRODUTOS ADICIONADOS");
-            Console.WriteLine("**************************");
-            Console.WriteLine();
-
-            for (int i = 0; i < listaDeProdutos.Count; i++)
+            void registrarVenda()
             {
-                Console.WriteLine($"Produto {(i + 1)}:");
-                Console.WriteLine($"Nome: {listaDeProdutos[i].GetProduto()}");
-                Console.WriteLine($"Valor: R${listaDeProdutos[i].GetValor()}");
-                Console.WriteLine();
+                Vendas venda = new Vendas(listaDeProdutos);
+                venda.Registrar();
             }
 
-            Console.WriteLine();
-            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-            Console.ReadKey();
-            Console.Clear();
-        }
-
-
-        //fim dos voids
-
-        // Início do código
-        bool continuarSwitch = true;
-        while (continuarSwitch)
-        {
-            menu();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.Write("Digite uma opção: ");
-            int escolhaMenu = Convert.ToInt32(Console.ReadLine());
-            switch (escolhaMenu)
+            void clientes()
             {
-                case 1:
-                    Console.WriteLine("Opção 1 escolhida");
-                    Thread.Sleep(2000);
-                    break;
-                case 2:
-                    Console.WriteLine("Opção 2 escolhida");
-                    Thread.Sleep(2000);
-                    cadastro();
-                    break;
-                case 3:
-                    Console.WriteLine("Opção 3 escolhida");
-                    adicionarProduto();
-                    break;
-                case 4:
-                    Console.WriteLine("Opção 4 escolhida");
-                    break;
-                case 5:
-                    Console.WriteLine("Opção 4 escolhida");
-                    clientes();
-                    break;
-                case 6:
-                    produtos();
-                    break;
-                case -1:
-                    continuarSwitch = false;
-                    Console.Clear();
+                Console.Clear();
+                Console.WriteLine("**************************");
+                Console.WriteLine("LISTA DE CLIENTES ADICIONADOS");
+                Console.WriteLine("**************************");
+                Console.WriteLine();
+
+                for (int i = 0; i < listaDeClientes.Count; i++)
+                {
+                    Console.WriteLine($"Cliente {(i + 1)}:");
+                    Console.WriteLine($"Nome: {listaDeClientes[i].GetNome()}");
+                    Console.WriteLine($"Telefone: {listaDeClientes[i].GetTelefone()}");
+                    Console.WriteLine($"Email: {listaDeClientes[i].GetEmail()}");
                     Console.WriteLine();
-                    Console.WriteLine("Opção -1 escolhida");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            void cadastro()
+            {
+                Console.Clear();
+                // Criação de um novo cliente
+                cadastroCliente novoCliente = new cadastroCliente();
+
+                // Nome
+                Console.WriteLine("Qual cliente você gostaria de registrar ?");
+                novoCliente.SetNome(Console.ReadLine());
+                Console.WriteLine();
+
+                // Telefone
+                Console.WriteLine("Qual o telefone do cliente:");
+                novoCliente.SetTelefone(Console.ReadLine());
+                Console.WriteLine();
+
+                // Email
+                Console.WriteLine("Qual o email do cliente:");
+                novoCliente.SetEmail(Console.ReadLine());
+                Console.WriteLine();
+
+                // Adiciona o novo cliente à lista de clientes
+                listaDeClientes.Add(novoCliente);
+            }
+
+            void adicionarProduto()
+            {
+                Console.Clear();
+
+                estoque novoProduto = new estoque();
+
+                // Nome do produto
+                Console.WriteLine("Qual produto você gostaria de adicionar?");
+                novoProduto.SetProduto(Console.ReadLine());
+                Console.WriteLine();
+
+                // Valor do produto
+                Console.WriteLine("Qual o valor do produto?");
+                novoProduto.SetValor(float.Parse(Console.ReadLine()));
+                Console.WriteLine();
+                Console.Clear();
+                Console.WriteLine($"Você adicionou o produto: {novoProduto.GetProduto()} o valor dele é R${novoProduto.GetValor()}");
+                Thread.Sleep(5000);
+
+
+                listaDeProdutos.Add(novoProduto);
+            }
+
+            void produtos()
+            {
+                Console.Clear();
+                Console.WriteLine("**************************");
+                Console.WriteLine("LISTA DE PRODUTOS ADICIONADOS");
+                Console.WriteLine("**************************");
+                Console.WriteLine();
+
+                for (int i = 0; i < listaDeProdutos.Count; i++)
+                {
+                    Console.WriteLine($"Produto {(i + 1)}:");
+                    Console.WriteLine($"Nome: {listaDeProdutos[i].GetProduto()}");
+                    Console.WriteLine($"Valor: R${listaDeProdutos[i].GetValor()}");
                     Console.WriteLine();
-                    Console.WriteLine("saindo do sistema");
-                    Thread.Sleep(5000);
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida:");
-                    Thread.Sleep(2000);
-                    menu();
-                    break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+
+
+
+            //fim dos voids
+
+            // Início do código
+            bool continuarSwitch = true;
+            while (continuarSwitch)
+            {
+                menu();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write("Digite uma opção: ");
+                int escolhaMenu = Convert.ToInt32(Console.ReadLine());
+                switch (escolhaMenu)
+                {
+                    case 1:
+                        if (listaDeProdutos.Count == 0)
+                        {
+                            Console.WriteLine("Não há produtos cadastrados. Por favor, cadastre produtos antes de registrar uma venda.");
+                            Thread.Sleep(2000);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Opção 1 escolhida");
+                            Thread.Sleep(2000);
+                            registrarVenda();
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Opção 2 escolhida");
+                        Thread.Sleep(2000);
+                        cadastro();
+                        break;
+                    case 3:
+                        Console.WriteLine("Opção 3 escolhida");
+                        adicionarProduto();
+                        break;
+                    case 4:
+                        Console.WriteLine("Opção 4 escolhida");
+                        break;
+                    case 5:
+                        Console.WriteLine("Opção 4 escolhida");
+                        clientes();
+                        break;
+                    case 6:
+                        produtos();
+                        break;
+                    case -1:
+                        continuarSwitch = false;
+                        Console.Clear();
+                        Console.WriteLine();
+                        Console.WriteLine("Opção -1 escolhida");
+                        Console.WriteLine();
+                        Console.WriteLine("saindo do sistema");
+                        Thread.Sleep(5000);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida:");
+                        Thread.Sleep(2000);
+                        menu();
+                        break;
+                }
             }
         }
     }
---
+++ ProjetoAlgoritmos/Vendas.cs
@@ -0,0 +1,72 @@
+﻿using Padaria;
+using System.Threading;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Padaria
+{
+    class Vendas
+    {
+        private List<estoque> produtos;
+        private List<(estoque produto, int quantidade)> carrinho;
+
+        public Vendas(List<estoque> produtos)
+        {
+            this.produtos = produtos;
+            this.carrinho = new List<(estoque, int)>();
+        }
+
+        public void Registrar()
+        {
+            Console.Clear();
+            bool adicionandoProdutos = true;
+
+            while (adicionandoProdutos)
+            {
+                Console.WriteLine("Produtos disponíveis:");
+                for (int i = 0; i < produtos.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {produtos[i].GetProduto()} - R${produtos[i].GetValor()}");
+                }
+
+                Console.WriteLine();
+                Console.Write("Digite o número do produto que deseja adicionar ao carrinho (ou -1 para finalizar): ");
+                int escolhaProduto = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                if (escolhaProduto == -2)
+                {
+                    adicionandoProdutos = false;
+                    continue;
+                }
+
+                if (escolhaProduto < 0 || escolhaProduto >= produtos.Count)
+                {
+                    Console.WriteLine("Produto inválido. Tente novamente.");
+                    continue;
+                }
+
+                Console.Write("Digite a quantidade: ");
+                int quantidade = Convert.ToInt32(Console.ReadLine());
+
+                carrinho.Add((produtos[escolhaProduto], quantidade));
+
+                Console.Clear();
+            }
+
+            Console.Clear();
+            float total = carrinho.Sum(item => item.produto.GetValor() * item.quantidade);
+            Console.WriteLine($"Total do carrinho: R${total}");
+            Console.WriteLine();
+            Console.Write("Digite o valor pago pelo cliente: ");
+            float valorPago = float.Parse(Console.ReadLine());
+
+            float troco = valorPago - total;
+            Console.WriteLine($"Troco: R${troco}");
+            Console.WriteLine();
+            Console.WriteLine("Venda finalizada. Obrigado!");
+
+            Thread.Sleep(5000);
+        }
+    }
+}
